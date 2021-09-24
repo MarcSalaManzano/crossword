e71f364d87c6d60467229370cf3a4840067142a7
Benhobels <Benhobels@users.noreply.github.com>
2021-09-24 12:24:09 +0200

    Read the small .txt and filter by word length

--- crossword.py
+++ crossword.py
@@ -1,4 +1,17 @@
-import numpy
 
-if __name__ == "__main__":
-    print("Hola")
+
+if __name__ == '__main__':
+    with open('diccionari_CB_v2.txt', 'r') as file:
+        lines = file.readlines()
+        dict = {}
+        for line in lines:
+            if len(line) not in dict:
+                dict[len(line)] = [line.strip('\n')]
+            else:
+                dict[len(line)].append(line.strip('\n'))
+
+        total = 0
+        for key, value in dict.items():
+            print(str(key-1) + ': ' + str(len(value)))
+            total += len(value)
+        print(total)
