cf74502b6a1d8eceb931022626f5627b3c9ccf02
Benhobels <Benhobels@users.noreply.github.com>
2021-10-02 20:06:54 +0200

    Backtracking skeleton done, missing check_restrictions

--- crossword.py
+++ crossword.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time
 
 def read_crossword_file(file_name):
     """
@@ -18,7 +18,7 @@ def read_crossword_file(file_name):
         crossword_row = 0
         for line in lines:
             result_variables = variable_horizontal_setup(line, crossword_row, len(variables))
-            if result_variables != []:
+            if result_variables:
                 variables.extend(result_variables)
             crossword_row += 1
 
@@ -29,7 +29,7 @@ def read_crossword_file(file_name):
             column = ''.join(column)
             if column.find('\t') == -1 and column.find('\n') == -1:
                 result_variables = variable_vertical_setup(column, crossword_column, len(variables))
-                if result_variables != []:
+                if result_variables:
                     variables.extend(result_variables)
                 crossword_column += 1
     file.close()
@@ -148,30 +148,36 @@ def create_collision_matrix(crossword_variables):
     :return collisions: np.array of tuples that mark the position of the characters that collide between 2 variables
     """
     num_of_vars = crossword_variables.shape[0]
-    collisions = np.empty([num_of_vars,num_of_vars], dtype=object)
+    collisions = np.empty([num_of_vars, num_of_vars], dtype=object)
     index = 0
     for variable in crossword_variables:
         neighbor_index = index
         if variable[1] == 0:
             variable_row = variable[2][0]
             variable_column = variable[2][1]
-            variable_columns = (variable_column, variable_column+variable[0]-1)
+            variable_columns = (variable_column, variable_column + variable[0] - 1)
             for neighbor_variable in crossword_variables[index:]:
                 neighbor_variable_row = neighbor_variable[2][0]
                 neighbor_variable_column = neighbor_variable[2][1]
-                neighbor_variable_rows = (neighbor_variable_row, neighbor_variable_row+neighbor_variable[0]-1)
-                if neighbor_variable[1] == 1 and (is_in_range(neighbor_variable_rows, variable_row) and is_in_range(variable_columns, neighbor_variable_column)):
-                    collisions[index, neighbor_index] = (neighbor_variable_column-variable_column,variable_row-neighbor_variable_row)
-                    collisions[neighbor_index, index] = (variable_row - neighbor_variable_row, neighbor_variable_column - variable_column)
+                neighbor_variable_rows = (neighbor_variable_row, neighbor_variable_row + neighbor_variable[0] - 1)
+                if neighbor_variable[1] == 1 and (
+                        is_in_range(neighbor_variable_rows, variable_row) and is_in_range(variable_columns,
+                                                                                          neighbor_variable_column)):
+                    collisions[index, neighbor_index] = (
+                        neighbor_variable_column - variable_column, variable_row - neighbor_variable_row)
+                    collisions[neighbor_index, index] = (
+                        variable_row - neighbor_variable_row, neighbor_variable_column - variable_column)
                 neighbor_index += 1
         index += 1
     return collisions
 
+
 def is_in_range(range, number):
     return (range[0] <= number) and (number <= range[1])
 
+
 def variable_degree_heuristic(not_assigned_variables, collision_matrix):
-    '''
+    """
     variable_degree_heuristic():
         1) selects the unassigned variable involved in a greater number of constraints
     Parameters:
@@ -179,30 +185,55 @@ def variable_degree_heuristic(not_assigned_variables, collision_matrix):
         2) collision_matrix (numpy array): contains the contraints relations between variables
     Returns:
         1) selected_variable (int): number of the selected variable that has the greatest number of constraints
-    '''
+    """
     selected_variable = None
     selected_variable_constraints = -1
     if collision_matrix.size != 0:
         for variable in not_assigned_variables:
             constraints_number = 0
             if variable[1] == 0:
-                for collision_value in collision_matrix[variable[3]-1]:
-                    if collision_value != None:
+                for collision_value in collision_matrix[variable[3] - 1]:
+                    if collision_value is not None:
                         constraints_number += 1
             else:
-                for collision_value in collision_matrix[variable[3]-1]:
-                    if collision_value != None:
+                for collision_value in collision_matrix[variable[3] - 1]:
+                    if collision_value is not None:
                         constraints_number += 1
             if selected_variable_constraints < constraints_number:
-                selected_variable = variable[3]
+                selected_variable = variable
                 selected_variable_constraints = constraints_number
-            print("Variable numero: " + str(variable[3]) + " - restriccions: " + str(constraints_number))
-    print("Variable seleccionada: " + str(selected_variable) + " amb un total de " + str(selected_variable_constraints) + " restriccions")
     return selected_variable
+
+
+def check_restrictions():
+    return True
+
+
+def backtracking(assigned, non_assigned, restrictions, domain):
+    if non_assigned.size == 0:
+        return assigned
+
+    variable_to_assign = variable_degree_heuristic(non_assigned, restrictions)
+    for domain_value in domain[variable_to_assign[0]]:  # 0 is the position of the size in the array
+        if check_restrictions():  # TODO: actualizar esto con lo que necesite las restricciones
+            new_assigned = np.append(assigned, [non_assigned[0], domain_value])  # Add to the assigned list the variable and their value
+            new_non_assigned = np.delete(non_assigned, 0, axis=0)
+            new_domain = domain
+            new_domain[variable_to_assign[0]] = np.delete(new_domain[variable_to_assign[0]], np.where(domain[variable_to_assign[0]] == domain_value))
+            res = backtracking(new_assigned, new_non_assigned, restrictions, new_domain)
+            if res is not None:
+                return res
+    return None
+
 
 if __name__ == '__main__':
     # obtain the variables present in the crossword
+    time0 = time.time()
     crossword_variables = read_crossword_file("crossword_CB_v2.txt")
     collision_matrix = create_collision_matrix(crossword_variables)
     word_dict = read_word_dictionary('diccionari_CB_v2.txt')
-    variable_to_assign = variable_degree_heuristic(crossword_variables, collision_matrix)
+    time1 = time.time()
+    print("tiempo setup" + str(time1-time0))
+    backtracking(np.array([]), crossword_variables, collision_matrix, word_dict)
+    time2 = time.time()
+    print("tiempo bt" + str(time2-time1))
